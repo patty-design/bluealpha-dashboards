786470a5671c0838e23e439001d1ba9725d6a8b3
Maverick <maverickforjesse@agentmail.to>
2026-03-10 18:35:13 +0000

    Auto-route root URL to dashboard based on subdomain

--- app.py
+++ app.py
@@ -36,6 +36,9 @@ def serve_static(filename):
 
 @app.route("/")
 def index():
+    host = request.host.split(".")[0].lower()
+    if host in DASHBOARDS:
+        return dashboard(host)
     return "Blue Alpha Dashboards", 200
 
 @app.route("/<name>")
